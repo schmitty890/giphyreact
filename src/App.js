da658696888bd8968be9a5f842f59cabfd764552
jschmitt <schmitty890@gmail.com>
2018-06-24 17:52:51 -0400

    displaying search results

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     };
   } 
 
   componentDidMount() {
-
+    this.performSearch();
   }
   
-  performSearch = (query) => {
+  performSearch = (query = 'cats') => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=h31f12EISLE8YkJ1XsRX5Yj8XZxNIgu4`)
       .then(response => {
         this.setState({
-          gifs: response.data.data
+          gifs: response.data.data,
+          loading: false
         });
       })
       .catch(error => {
@@ -40,7 +42,11 @@ export default class App extends Component {
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+          {
+            (this.state.loading)
+             ? <p>Loading...</p>
+             : <GifList data={this.state.gifs} />
+          }          
         </div>
       </div>
     );
